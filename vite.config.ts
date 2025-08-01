f764f0ac7870a15335f173931589c49d0c30f0be
Joy <joykarmakar987654321@gmail.com>
2025-08-02 00:32:01 +0530

    updated package

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,9 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(),],
+	plugins: [tailwindcss(), sveltekit()],
 	preview: {
 		host: '0.0.0.0', // Make the app accessible on all network interfaces
 		port: 4000 // Change this to any port you'd like for production preview
-	},
-	server: {
-		proxy: {
-			'/api': {
-				target: 'https://nodemailer-bay.vercel.app',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, '')
-			}
-		}
 	}
 });
