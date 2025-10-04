df0ae85f5884b875292ed47cccc54e9510e3a073
Joy <joykarmakar987654321@gmail.com>
2025-10-04 10:36:17 +0530

    update rendering layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,18 @@
 
 	import type { PageData } from './$types';
 	import { smoothScrollToSection } from '$lib/scroll';
+	import { onMount } from 'svelte';
 
 	export let data: PageData;
 	const { posts, meta, error } = data;
+
+	let contentVisible = false;
+
+	onMount(() => {
+		setTimeout(() => {
+			contentVisible = true;
+		}, 1500); // 1500ms = 1.5 seconds delay
+	});
 </script>
 
 <svelte:head>
@@ -32,37 +41,41 @@
 
 <Hero />
 
-<div class="container-auto">
-	<div class="flex justify-between border-b-2 border-black/80 pb-3">
-		<p class="font-medium text-2xl leading-[1.6] tracking-tighter">Featured work</p>
-		<a
-			on:click={(e) => smoothScrollToSection(e, '#project')}
-			href="#project"
-			class="text-lg font-medium flex items-center gap-1"
-		>
-			Scroll
-			<enhanced:img
-				loading="lazy"
-				src="/static/icons/arrow.svg"
-				alt="arrow"
-				class="animate-bounce h-4 w-4"
-			/>
-		</a>
+{#if contentVisible}
+	<div class="container-auto">
+		<div class="flex justify-between border-b-2 border-black/80 pb-3">
+			<p class="font-medium text-2xl leading-[1.6] tracking-tighter">Featured work</p>
+			<a
+				on:click={(e) => smoothScrollToSection(e, '#project')}
+				href="#project"
+				class="text-lg font-medium flex items-center gap-1"
+			>
+				Scroll
+				<enhanced:img
+					loading="lazy"
+					src="/static/icons/arrow.svg"
+					alt="arrow"
+					class="animate-bounce h-4 w-4"
+				/>
+			</a>
+		</div>
 	</div>
-</div>
 
-<div id="project"></div>
+	<div id="project"></div>
+{/if}
 
 <Project {posts} {error} />
 
-<div class="container-auto text-center mt-28">
-	<a
-		href="/works"
-		class="inline-flex items-center px-8 py-4 bg-gray-900 text-white text-lg font-medium rounded-full shadow-lg hover:bg-gray-700 transition-all duration-300 transform hover:-translate-y-1 hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-gray-300"
-	>
-		All works
-	</a>
-</div>
+{#if contentVisible}
+	<div class="container-auto text-center mt-28">
+		<a
+			href="/works"
+			class="inline-flex items-center px-8 py-4 bg-gray-900 text-white text-lg font-medium rounded-full shadow-lg hover:bg-gray-700 transition-all duration-300 transform hover:-translate-y-1 hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-gray-300"
+		>
+			All works
+		</a>
+	</div>
+{/if}
 
 <About />
 
